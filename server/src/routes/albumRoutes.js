563ce12d4a3b49f80b0e3a5f2d092167c5fe12c3
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-08 18:43:34 +0500

    ✨ feat(album): implement album routes and controllers
    - add get all albums and get album by id routes
    - implement controllers to handle album data retrieval

--- server/src/routes/albumRoutes.js
+++ server/src/routes/albumRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { getAlbumById, getAllAlbums } from "../controllers/AlbumControllers.js";
 const albumRoutes = express.Router();
 
-albumRoutes.get('/', (req, res) => {});
+albumRoutes.get('/', getAllAlbums);
+albumRoutes.get('/:albumId', getAlbumById);
 
 export default albumRoutes;
