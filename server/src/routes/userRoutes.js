680aff01ca9a0463fd4ab0488fc39fb60cd9ec8c
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-08 19:32:21 +0500

    ✨ feat(user): implement get all users endpoint
    - create user controller to fetch all users except the current user
    - add route to get all users with authentication middleware

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { getAllUsers } from "../controllers/userControllers.js";
 const userRoutes = express.Router();
 
-userRoutes.get('/', (req, res) => {});
+userRoutes.get('/', protectRoute, getAllUsers);
 
 export default userRoutes;
