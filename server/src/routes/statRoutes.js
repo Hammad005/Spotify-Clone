3d2a23a7fd67dc6ef565ae7185136514982729f4
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-08 19:45:45 +0500

    ✨ feat(stat): implement statistics endpoint for admin users
    - add endpoint to retrieve total counts of songs, users, and albums
    - calculate the number of unique artists from songs and albums collections
    - protect the route with admin authentication middleware

--- server/src/routes/statRoutes.js
+++ server/src/routes/statRoutes.js
@@ -1,6 +1,32 @@
 import express from "express";
+import User from "../models/User.js";
+import Song from "../models/Song.js";
+import Album from "../models/Album.js";
+import { protectRoute, requireAdmin } from "../middleware/authMiddleware.js";
 const statRoutes = express.Router();
 
-statRoutes.get('/', (req, res) => {});
+statRoutes.use(protectRoute, requireAdmin)
+
+statRoutes.get('/', async (req, res, next) => {
+    try {
+
+        const [totalSongs, totalUsers, totalAlbums, uniqueArtists] = await Promise.all([
+            Song.countDocuments(),
+            User.countDocuments(),
+            Album.countDocuments(),
+
+            Song.aggregate([
+                { $unionWith: { coll: "albums", pipeline: [] } },
+                { $group: { _id: "$artist" } },
+                { $count: "uniqueArtists" }
+            ])
+        ]);
+
+
+        res.status(200).json({ success: true, data: { totalSongs, totalAlbums, totalUsers, uniqueArtists: uniqueArtists[0]?.count || 0 } });
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default statRoutes;
