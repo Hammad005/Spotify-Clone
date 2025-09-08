7b8a5c7d087efde9dfdfff88248bcca8be27b80a
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-08 19:58:59 +0500

    ✨ feat(app): add react router and basic pages
    - install react-router-dom
    - create home and auth-callback pages
    - configure react router in app.tsx
    - wrap app with browserrouter in main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home/HomePage"
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
+
 const App = () => {
   return (
     <>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
+    </Routes>
     </>
   )
 }
